c50fbd10a2292c29e7ac1a89e1a578b12dd72a88
ajayg51 <ajayg.ug18.cs@nitp.ac.in>
2024-02-28 03:36:04 +0530

    initial setup

---
+++ src/test/java/com/example/nymble_assignment/MainTest.java
@@ -0,0 +1,17 @@
+package com.example.nymble_assignment;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertFalse(true);
+    }
+}
